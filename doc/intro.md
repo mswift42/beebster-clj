8520ab79ea3618223a1a1c818493328e4682a126
mswift42 <martin.haesler@gmail.com>
2013-10-21 10:50:11 +0100

    initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to beebster-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/beebster_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns beebster-clj.core-test
+  (:require [clojure.test :refer :all]
+            [beebster-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
