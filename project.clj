4072b76d18f63e3df4f94ebd5912779994e3e70b
mswift42 <martin.haesler@gmail.com>
2013-10-26 09:38:10 +0100

    Added get_info function.

--- project.clj
+++ project.clj
@@ -3,4 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [enlive "1.1.1"]
+                 [ring "1.2.0"]
+                 [compojure "1.1.5"]
+                 [net.cgrand/moustache "1.1.0"]
+  ]
+  :dev-dependencies [[lein-ring "0.4.5"]]
+  )
