56e771e166d396dcf9fa50840746311b5cb07f6e
Martin Haesler <martin.haesler@gmail.com>
2014-01-19 09:01:47 +0000

    Add beebster categories.

--- src/beebster_clj/views.clj
+++ src/beebster_clj/views.clj
@@ -1,6 +1,10 @@
 (ns beebster-clj.views
   (:use hiccup.core hiccup.util hiccup.page))
 
+(def categories
+  '("search" "popular" "highlights" "films" "nature"
+    "crime" "sitcom" "sport" "thriller"))
+
 (defn header
   "universal bootstrap header for all beebster sites."
   [links]
@@ -11,7 +15,9 @@
       [:div.nav-collapse.collapse
        [:ul.nav
         (for [[url description] links]
-          [:li.active [:a {:href url} description]])]]]]]))
+          [:li.active [:a {:href url} description]])
+        (for [link categories]
+          [:li.active [:a {:href (apply str "/categories?category=" link)} link]])]]]]]))
 
 (defn base-template
   "base html template for all beebster sites."
@@ -21,9 +27,36 @@
     [:title title]
     (include-css "/css/bootstrap.min.css")]))
 
+
+
+
+
+(defmacro category-template 
+  "macro for category links."
+  [url cat header]
+  `(base-template
+    (:title ~header)
+    (for [i categories]
+      (html ([:a.ms {:href (apply str "/" i)} i])))
+    (:h3 :id "header" ,header)
+    (display-results (search-categories ~header))))
+
+
 (defn index-page
   "html for index page"
   []
   (html
    (base-template "Index")
    (header '(("/about" "About")))))
+
+(defn about-page
+  []
+  (html
+   (base-template "About beebster")
+   (header '(("/" "Index")))
+   [:h2.header "Beebster"]
+   [:br]
+   [:p "Beebster, a Web Gui for "]
+   [:a {:href "http://www.infradead.org/get_iplayer/html/get_iplayer.html"}
+    "get-iplayer"]))
+
