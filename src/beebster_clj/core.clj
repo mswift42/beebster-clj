0828bb54bd8b0f14e1fdde8c21afa6973b727448
Martin Haesler <martin.haesler@gmail.com>
2014-01-19 18:17:35 +0000

    Add delete string.

--- src/beebster_clj/core.clj
+++ src/beebster_clj/core.clj
@@ -1,29 +1,26 @@
 (ns beebster-clj.core
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
-            [beebster-clj.views :as views])
+            ;; [beebster-clj.views :as views]
+            )
   (:use [ring.adapter.jetty :only [run-jetty]]
         [clojure.java.shell :only [sh]]
         [clojure.string :as str :only [split-lines ]]
-        hiccup.core hiccup.util hiccup.page compojure.core))
+        hiccup.core hiccup.util hiccup.page compojure.core ))
 
 
 (def iplayer-command
   '("get_iplayer" "--nocopyright" "--limitmatches" "50" "--listformat" "<index> <pid> <thumbnail> <name> <episode>"))
 
-(defn iplayer-search
-  "run external command with given args list."
-  [search-title]
-  (split-lines (:out (apply sh (flatten
-                                (conj (list search-title) iplayer-command))))))
+
 
 (def iplayer-info
   '("get_iplayer" "-i"))
 
-(defn old-recordings? 
-  "Does get-iplayer complain about recorded programmes > 30 days?"
-  [str]
-  (remove nil? (map #(re-find #"These programmes should be deleted:" %) str)))
+(def delete-string
+  "These programmes should be deleted:")
+
+
 
 (defn get-info
   "get-iplayer info for entered index"
@@ -46,19 +43,45 @@
   [s]
   (remove #(= "" %) (map #(re-find #"^[0-9]*" %) s)))
 
+(defn old-recordings? 
+  "Does get-iplayer complain about recorded programmes > 30 days?"
+  [str]
+  (remove nil? (map #(re-find #"These programmes should be deleted:" %) str)))
 
 (defn iplayer-download-command
   "concatenate index and mode to download command"
    [index mode]
   (apply str  "get_iplayer " mode "1" " -g  --nocopyright --output=\"$HOME/Videos\"" " " index)) 
  
+(defn get-url
+  "return /result url string concatenated with index"
+  [index]
+  (apply str "/result?index=" index))
+
+(defn iplayer-search-resultstring
+  "run external command with given args list."
+  [search-title]
+  (:out (apply sh (flatten
+                   (conj (list search-title) iplayer-command)))))
+
+
+
+(defn iplayer-search
+  "run external command with given args list."
+  [search-title]
+  (let [result (iplayer-search-resultstring search-title)]
+    (if (old-recordings? (split-lines result))
+      (split-lines (subs result (.indexOf result delete-string)))
+      (split-lines result))))
+
 
 (defn search-categories
   "search iplayer for a given category"
   [cat]
-  (split-lines (:out (apply sh (flatten
-                                (conj (list "--category" cat)
-                                      iplayer-command))))))
+  (drop 1 (butlast
+           (split-lines (:out (apply sh (flatten
+                                         (conj (list "--category" cat)
+                                               iplayer-command))))))))
 
 
 
