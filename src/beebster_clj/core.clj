e7ecf41c777dc277912f9651598beb0dc48d8bd7
Martin Haesler <martin.haesler@gmail.com>
2014-01-20 20:20:43 +0000

    Add download-modes.

--- src/beebster_clj/core.clj
+++ src/beebster_clj/core.clj
@@ -2,6 +2,7 @@
   (:import [java.lang.ProcessBuilder])
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
+            
             ;; [beebster-clj.views :as views]
             )
   (:use [ring.adapter.jetty :only [run-jetty]]
@@ -27,6 +28,15 @@
   [index]
   (split-lines (:out (apply sh (flatten (conj (list index) iplayer-info))))))
 
+(defn get-download-modes
+  "build list of possible download-modes for a given index."
+  [lst]
+  (let [modes (first (filter #(re-find #"modes.*" %) lst))]
+    (remove nil? (list (re-find #"flashhigh" modes)
+                       (re-find #"flashvhigh" modes)
+                       (re-find #"flashhd" modes)
+                       (re-find #"flashlow" modes)))))
+
 (defn load-thumbandinfo-for-index
   "grep url for thumbnail size44, title and description
    for entered index."
@@ -35,7 +45,8 @@
     (list
      (map #(re-find #"htt.*" %) (filter #(re-find #"thumbnail4.*" %) ind))
      (map #(re-find #"[A-Z].*" %) (filter #(re-find #"desc:.*" %) ind))
-     (map #(re-find #"[A-Z0-9].*" %) (filter #(re-find #"title:.*" %) ind)))))
+     (map #(re-find #"[A-Z0-9].*" %) (filter #(re-find #"title:.*" %) ind))
+     (get-download-modes ind))))
 
 (defn get-thumb-from-search
   "return list of thumbnail for searchterm."
@@ -53,18 +64,6 @@
   [s]
   (remove #(= "" %) (map #(re-find #"^[0-9]*" %) s)))
 
-(defn get-download-modes
-  "build list of possible download-modes for a given index."
-  [lst]
-  (let [modes (first (filter #(re-find #"modes.*" %) lst))]
-    (remove nil? (list (re-find #"flashhigh" modes)
-                       (re-find #"flashvhigh" modes)
-                       (re-find #"flashhd" modes)
-                       (re-find #"flashlow" modes)))))
-
-
-
-
 (defn old-recordings? 
   "Does get-iplayer complain about recorded programmes > 30 days?"
   [lst]
@@ -75,11 +74,8 @@
 (defn iplayer-download-command
   "concatenate index and mode to download command"
    [index mode]
-   (list "get-iplayer" (apply str "modes=" mode "1")
-         (apply str "output=" (System/getenv "HOME") "/Videos/") "-g" index)) 
-
-
-
+   (list "get-iplayer" (apply str "modes=" mode "1\"")
+         "output=\"$HOME/Videos\"" "-g" index)) 
 
 (defn get-url
   "return /info url string concatenated with index"
@@ -92,16 +88,15 @@
   (:out (apply sh (flatten
                    (conj (list search-title) iplayer-command)))))
 
-
-
 (defn iplayer-search
-  "run external command with given args list."
+  "If old-recording? display a list of all programmes,
+   that have to be deleted, otherwise, return a list of 
+   all matching programmes for search input."
   [search-title]
   (let [result (iplayer-search-resultstring search-title)]
     (if (old-recordings? (split-lines result))
       (split-lines (subs result (.indexOf result delete-string)))
       (split-lines result))))
-
 
 (defn search-categories
   "search iplayer for a given category"
