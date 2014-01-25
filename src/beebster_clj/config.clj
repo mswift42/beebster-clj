229f01427977612b361e014c6a34aec01f933043
Martin Haesler <martin.haesler@gmail.com>
2014-01-25 09:16:47 +0000

    Add header to config.

--- src/beebster_clj/config.clj
+++ src/beebster_clj/config.clj
@@ -1,7 +1,16 @@
+;; config file for beebster.
+;; Replace gnome-terminal with whatever terminal program you prefer to use.
+;; However, make sure to adapt the working-directory argument.
+;; Beebster will work without it, but all downloaded programmes will then be
+;; dumped into the beebster-clj folder.
+
 (ns beebster-clj.config)
 
 (def config {:terminal "gnome-terminal"
-             :download-folder (apply str "--working-directory=" (System/getenv "HOME") "/Videos")})
+             :download-folder
+             (apply str "--working-directory=" (System/getenv "HOME") "/Videos")})
+
+
 
 
 
